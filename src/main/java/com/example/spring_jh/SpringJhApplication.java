18a0b72e8928192f4c6866b6d66e260a93b4f69d
hee0hee <132984807+hee0hee@users.noreply.github.com>
2023-06-15 21:35:48 +0900

    SQL 연결, table 생성

---
+++ src/main/java/com/example/spring_jh/SpringJhApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_jh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJhApplication.class, args);
+	}
+
+}
